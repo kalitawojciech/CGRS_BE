a6a33c4c09e7006afdda7f9b44af04cf86c67cfe
kalitawojciech <wojtek1311997@gmail.com>
2021-01-24 22:31:25 +0100

    added BadRequestException and NotFoundException

---
+++ src/CGRS.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CGRS.Domain.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(Dictionary<string, List<string>> errors)
+        {
+            this.Data.Add("errors", errors);
+        }
+    }
+}
