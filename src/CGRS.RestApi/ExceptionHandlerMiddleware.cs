387117c307c846e60a1a4a30e1e38414a7af80d4
kalitawojciech <wojtek1311997@gmail.com>
2021-01-24 22:43:00 +0100

    added exception handling middleware

---
+++ src/CGRS.RestApi/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using CGRS.Domain.Exceptions;
+
+using Microsoft.AspNetCore.Http;
+
+using Newtonsoft.Json;
+
+namespace CGRS.RestApi
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var exceptionType = exception.GetType();
+            var statusCode = HttpStatusCode.InternalServerError;
+            var status = string.Empty;
+            var payload = string.Empty;
+
+            switch (exception)
+            {
+                case NotFoundException e:
+                    statusCode = HttpStatusCode.NotFound;
+                    status = "NOT_FOUND";
+                    context.Response.StatusCode = (int)statusCode;
+                    context.Response.ContentType = "application/json";
+                    return context.Response.WriteAsync(payload);
+
+                case BadRequestException e:
+                    statusCode = HttpStatusCode.BadRequest;
+                    status = "BAD_REQUEST";
+                    break;
+
+                default:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    context.Response.StatusCode = (int)statusCode;
+                    context.Response.ContentType = "application/json";
+                    return context.Response.WriteAsync(exception.Message);
+            }
+
+            var response = new
+            {
+                errors = exception.Data["errors"],
+                status,
+                timeStamp = DateTime.Now,
+                message = exception.Source + " error",
+            };
+
+            payload = JsonConvert.SerializeObject(response);
+            context.Response.StatusCode = (int)statusCode;
+            context.Response.ContentType = "application/json";
+
+            return context.Response.WriteAsync(payload);
+        }
+    }
+}
