30a74972cc6f75dcbb6149c249ebd5efd14b2eee
kalitawojciech <wojtek1311997@gmail.com>
2021-01-25 23:32:35 +0100

    created UserController

---
+++ src/CGRS.RestApi/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CGRS.RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        #region Queries
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllUsersourses()
+        {
+            return Ok();
+        }
+
+        #endregion
+    }
+}
