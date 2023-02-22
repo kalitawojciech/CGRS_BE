c49f732f6cf63866c98c040af7445698d5b103c7
kalitawojciech <wojtek1311997@gmail.com>
2023-02-22 21:26:22 +0100

    created TagsController

---
+++ src/CGRS.RestApi/Controllers/TagsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CGRS.RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TagsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TagsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
