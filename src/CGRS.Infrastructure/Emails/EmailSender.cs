0366c95612bf590626bd0b062995ec32d15ab41c
kalitawojciech <wojtek1311997@gmail.com>
2023-08-08 23:12:33 +0200

    email sender

---
+++ src/CGRS.Infrastructure/Emails/EmailSender.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace CGRS.Infrastructure.Emails
+{
+    public class EmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string recipientEmail, string subject, string message)
+        {
+
+        }
+    }
+}
